4a597ff00081f8407e62cb839325a9a3be38900c
Nick Sullivan-Friedman <rpgcubed@gmail.com>
2020-11-30 15:45:03 -0800

    feat: puzzles

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 const axios = require('axios');
+const DOMParser = require('dom-parser');
+const CronJob = require('cron').CronJob;
 
 const { Client, MessageEmbed } = require('discord.js');
 
@@ -32,8 +34,6 @@ const refreshLeaderboard = async () => {
   const localScores = sortedMembers.map(member => member.local_score);
   const stars = sortedMembers.map(member => member.stars);
 
-
-
   const embed = new MessageEmbed()
     .setTitle(data.event)
     .addFields(
@@ -46,10 +46,81 @@ const refreshLeaderboard = async () => {
   leaderboardChannel.send(embed);
 }
 
+const getNewProblem = async () => {
+  console.log('GETTING NEWEST PROBLEM');
+  const today = new Date();
+  const day = 1//today.getUTCDate();
+  const year = 2019//today.getFullYear();
+  const uri = `https://adventofcode.com/${year}/day/${day}`;
+  const problemResponse = await axios.get(uri);
+  if(problemResponse.status !== 200){
+    return;
+  }
+  const {data} = problemResponse;
+
+  const parser = new DOMParser();
+  const htmlDoc = parser.parseFromString(data, 'text/html');
+
+  const dayDescription = htmlDoc.getElementsByClassName('day-desc')[0];
+
+  const parse = (topNode, inCode = false) => {
+    if(topNode.childNodes === undefined){
+      return topNode.text || '';
+    }
+    return topNode.childNodes.reduce((acc, node) => {
+      switch(node.nodeName){
+        case 'h2':
+          return acc + '​[**' + parse(node, inCode) + `**](${uri})` + '\n';
+        case 'em':
+          if(inCode){
+            return acc + '`**`' + parse(node, inCode) + '`**`';
+          }
+          return acc + '**' + parse(node, inCode) + '**';
+        case 'li':
+          return acc + '  -  ' + parse(node, inCode);
+        case 'a':
+          console.log(node) 
+          return acc + `[${parse(node, inCode)}](${node.attributes.find(a=>a.name==='href').value})`;
+        case 'pre':
+          return acc + '\n' + parse(node, inCode);
+        case 'p':
+        case 'ul':
+        case 'span':
+        case '#text':
+          return acc + parse(node, inCode);
+        case 'code':
+          return acc + '`\u200b' + parse(node, true) + '\u200b`';
+        default:
+          return acc; 
+      }
+    },'');
+  };
+
+  const content = parse(dayDescription)
+    + `[Click here for your input file!](${uri}/input)\n`
+    + `[Click here to submit your response!](${uri})\n`;
+  console.log(dayDescription)
+
+  const chunksArr = content.match(/[\s\S]{1,2047}[\n]/g); // chunk into 2048 max chunks
+  chunksArr.forEach(description => {
+    problemChannel.send(
+    {
+      embed: {
+        description
+      }
+    })
+  })
+}
+
+
 client.on('ready', async () => {
   console.log('READY');
+
   leaderboardChannel = await client.channels.fetch(process.env.LEADERBOARD_CHANNEL_ID);
-  client.setInterval(refreshLeaderboard, delay);
+  // client.setInterval(refreshLeaderboard, delay);
+  // 
+  problemChannel = await client.channels.fetch(process.env.PROBLEM_CHANNEL_ID);
+  getNewProblem();
 });
 
 client.login();
